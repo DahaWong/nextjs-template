20bdf559d62c6e3d47eac9f2ef38357e7c52bd37
DahaWong <dahawong@gmail.com>
2022-12-01 23:36:31 +0800

    Try nextjs v13

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+import './globals.css';
+
+export default function Layout({children}: {children: React.ReactNode}) {
+  return (
+    <html lang="en">
+      <body className="flex flex-col justify-center p-20">
+        <h1 className="text-3xl font-bold underline">Nextjs Template</h1>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,4 @@
+// Not Found UI components do not accept any parameters.
+export default function NotFound() {
+  return "404 - Not Found";
+}
